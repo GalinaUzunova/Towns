b39011b1fd040cc3310212089f35b3eb337c359a
gbuzu <gbuzunova13@gmail.com>
2025-07-29 17:13:35 +0200

    Fix

--- towns.js
+++ towns.js
@@ -20,8 +20,9 @@ function deleteTown() {
 	else
 		$('#result').text(townName + " not found.");
 
-	function addTown() {
+	
+}
+function addTown() {
 		 let townName = $('#townNameForAdd').val();
 		  $('#townNameForAdd').val(''); $('#towns').append($('<option>').text(townName));
 		   $('#result').text(townName + " added."); }
-}
